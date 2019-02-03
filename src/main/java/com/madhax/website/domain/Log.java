f59b17579a1a19715a8a18c5ca39273b243f8d87
James Cathcart <cathcart.jm@gmail.com>
2019-02-03 16:26:39 -0700

    Created Log entity. closes #3

--- src/main/java/com/madhax/website/domain/Log.java
+++ src/main/java/com/madhax/website/domain/Log.java
@@ -1,8 +1,40 @@
 package com.madhax.website.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Log {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String message;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
