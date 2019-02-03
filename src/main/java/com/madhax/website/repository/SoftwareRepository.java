18e6b2c942ff68d3cf2066c16cd49d551f5dff1d
James Cathcart <cathcart.jm@gmail.com>
2019-02-03 16:33:00 -0700

    Created Software repository

---
+++ src/main/java/com/madhax/website/repository/SoftwareRepository.java
@@ -0,0 +1,10 @@
+package com.madhax.website.repository;
+
+import com.madhax.website.domain.Software;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SoftwareRepository extends CrudRepository<Software, Long> {
+
+
+
+}
