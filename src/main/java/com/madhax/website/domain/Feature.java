d6a40a3f38e8c578c65ff93ec4375ac943d3a844
James Cathcart <cathcart.jm@gmail.com>
2019-02-03 16:28:43 -0700

    Created Feature entity. closes #4

--- src/main/java/com/madhax/website/domain/Feature.java
+++ src/main/java/com/madhax/website/domain/Feature.java
@@ -1,7 +1,40 @@
 package com.madhax.website.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Feature {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String body;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
