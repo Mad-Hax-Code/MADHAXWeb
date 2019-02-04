7e82d4ca78a9491b8775a49f6b50edbde379ab38
James Cathcart <cathcart.jm@gmail.com>
2019-02-04 11:26:48 -0700

    Created individual view for Project. closes #14

--- src/main/java/com/madhax/website/controller/SoftwareController.java
+++ src/main/java/com/madhax/website/controller/SoftwareController.java
@@ -1,10 +1,17 @@
 package com.madhax.website.controller;
 
+import com.madhax.website.domain.Bug;
+import com.madhax.website.domain.Feature;
+import com.madhax.website.domain.Log;
+import com.madhax.website.domain.Project;
 import com.madhax.website.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Set;
 
 @Controller
 public class SoftwareController {
@@ -20,5 +27,20 @@ public class SoftwareController {
     public String software(Model model) {
         model.addAttribute("projects", projectRepository.findAll());
         return "software/softwareList";
+    }
+
+    @GetMapping("/software/{id}")
+    public String project(Model model, @PathVariable Long id) {
+
+        Project project = projectRepository.findById(id).get();
+        Set<Feature> features = project.getFeatures();
+        Set<Log> devLog = project.getDevLog();
+        Set<Bug> bugs = project.getBugs();
+
+        model.addAttribute("project", project);
+        model.addAttribute("features", features);
+        model.addAttribute("devLog", devLog);
+
+        return "software/project";
     }
 }
