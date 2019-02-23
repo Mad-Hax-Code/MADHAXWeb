c3627235fb80df6e9b07db5b2b4815615d0334ff
James Cathcart <cathcart.jm@gmail.com>
2019-02-23 11:35:23 -0700

    Getting up to date in order to switch branches

--- src/main/java/com/madhax/website/controller/NoteController.java
+++ src/main/java/com/madhax/website/controller/NoteController.java
@@ -1,4 +1,12 @@
 package com.madhax.website.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/project/{projectId}/note")
 public class NoteController {
+
+
+
 }
