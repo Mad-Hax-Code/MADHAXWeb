f0f1384271dc4db73349c085a9160af934c1b244
piegarden@gmail.com <40525096+piegarden1@users.noreply.github.com>
2019-02-23 15:24:50 -0600

    Tried implimenting an add new notes button, but running into errors

--- src/main/java/com/madhax/website/controller/NoteController.java
+++ src/main/java/com/madhax/website/controller/NoteController.java
@@ -1,11 +1,49 @@
 package com.madhax.website.controller;
 
+import com.madhax.website.domain.IssueType;
+import com.madhax.website.domain.Note;
+import com.madhax.website.domain.Project;
+import com.madhax.website.service.NoteService;
+import com.madhax.website.service.ProjectService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/project/{projectId}/note")
 public class NoteController {
+
+    protected final String CONFIRM_DELETE_URL = "project/note/confirmDelete";
+    protected final String NOTE_FORM_URL = "project/note/noteForm";
+
+    private final ProjectService projectService;
+    private final NoteService noteService;
+
+    public NoteController(ProjectService projectService, NoteService noteService) {
+        this.projectService = projectService;
+        this.noteService = noteService;
+    }
+
+    @GetMapping("/new")
+    public String newIssue(@PathVariable Long projectId, Model model){
+        model.addAttribute("note", new Note());
+        model.addAttribute("projectId", projectId);
+        return NOTE_FORM_URL;
+    }
+
+    @PostMapping("/new")
+    public String saveNewNote(@PathVariable Long projectId, @ModelAttribute Note note) {
+        Project project = projectService.getById((projectId));
+        project.addNote(note);
+        projectService.save(project);
+        return "redirect:/project/" + project.getId();
+    }
+
+    @GetMapping("/{noteId}/edit")
+    public String editIssue(@PathVariable Long noteId, Model model) {
+        model.addAttribute("note", noteService.getById(noteId));
+        return NOTE_FORM_URL;
+    }
 
 
 
