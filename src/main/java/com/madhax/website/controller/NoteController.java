0ed2cd339898098d9ed047bb2c120623c2cbff65
piegarden@gmail.com <40525096+piegarden1@users.noreply.github.com>
2019-02-23 18:31:39 -0600

    having issues deleting notes, post isnt supported it says

--- src/main/java/com/madhax/website/controller/NoteController.java
+++ src/main/java/com/madhax/website/controller/NoteController.java
@@ -5,6 +5,8 @@ import com.madhax.website.domain.Note;
 import com.madhax.website.domain.Project;
 import com.madhax.website.service.NoteService;
 import com.madhax.website.service.ProjectService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/project/{projectId}/note")
 public class NoteController {
+
+    private final Logger log = LoggerFactory.getLogger(NoteController.class);
 
     protected final String CONFIRM_DELETE_URL = "project/note/confirmDelete";
     protected final String NOTE_FORM_URL = "project/note/noteForm";
@@ -40,11 +44,30 @@ public class NoteController {
     }
 
     @GetMapping("/{noteId}/edit")
-    public String editIssue(@PathVariable Long noteId, Model model) {
+    public String editNote(@PathVariable Long noteId, Model model) {
         model.addAttribute("note", noteService.getById(noteId));
         return NOTE_FORM_URL;
     }
 
+    @PostMapping("/edit")
+    public String savedEditedNote(@ModelAttribute Note note, @PathVariable Long projectId) {
+        log.debug("Updating note ID: {} for project: {}", note.getId(), projectId);
+        Note savedNote = noteService.save(note);
+        return "redirect:/project/" + projectId;
+    }
+
+    @GetMapping("/{noteId}/delete")
+    public String confirmDeleteById(@PathVariable Long noteId, Model model) {
+        log.debug("Delete confirmation for note ID: {}", noteId);
+        model.addAttribute("note", noteService.getById(noteId));
+        return CONFIRM_DELETE_URL;
+    }
+
+    @PostMapping("/delete")
+    public String handleDeleteNoteById(@PathVariable Long projectId, @RequestParam Long noteId) {
+        noteService.deleteById(noteId);
+        return "redirect:/project/" + projectId;
+    }
 
 
 }
